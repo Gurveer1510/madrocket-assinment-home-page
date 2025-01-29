736a05f24bd4990060d946444f70d909931741df
Gurveer1510 <singhgurveer4691@gmail.com>
2025-01-29 11:56:43 +0000

    fix

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -9,7 +9,7 @@ import {getAuth} from "firebase/auth"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyA-mFa7XO6lxMWTh7_XNM2BNukfJ9eus4k",
+    apiKey: import.meta.env.VITE_API_KEY,
     authDomain: "madrocket-assignment-e81b2.firebaseapp.com",
     projectId: "madrocket-assignment-e81b2",
     storageBucket: "madrocket-assignment-e81b2.firebasestorage.app",
